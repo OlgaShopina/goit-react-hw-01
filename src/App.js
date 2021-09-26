6cfc335ce1f10473956f3f30bc9051c85a17a0bd
OlgaShopina <olga.shopina@gmail.com>
2021-09-26 20:53:35 +0300

    react-hw-01

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import Profile from './components/Profile';
+import Statistics from './components/Statistics';
+import FriendList from './components/FriendList';
+import Transactions from './components/Transactions';
+
+import user from './data/user.json';
+import statisticalData from './data/statistical-data.json';
+import friendList from './data/friend-list.json';
+import transactions from './data/transactions.json';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+    <Profile
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    <Statistics title="Upload stats" stats={statisticalData} />
+    <FriendList friends={friendList} />
+   <Transactions items={transactions} />
     </div>
   );
 }
